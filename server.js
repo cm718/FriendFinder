0c46da3b66dd7b10a199d8e5474bc35f431215e2
cm718 <cm718@icloud.com>
2019-01-17 14:35:45 -0600

    set up the serve.js with routing and PORT listener

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
+// Require in my dependencies
 const express = require('express');
 const mysql = require('mysql');
+// Assign express() and create server
 const app = express(); 
 
+// Set up the PORT
+const PORT = process.env.PORT || 8080;
+
+// Middleware for Express to parse data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Require in the api and html routes 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Tell the PORT to listen
+app.listen(PORT, ()=> {
+    console.log(`Listening on PORT:${PORT}`);
+});
