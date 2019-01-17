9bdb03e2ae2e26318212f87e086033b717209fa6
cm718 <cm718@icloud.com>
2019-01-17 14:52:44 -0600

    Added basic starter data for friendsData

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+
+const friendsData = [
+    {
+        "name": "Chris",
+        "photo": "https://images.google.com",
+        "scores": [5,3,4,3,2,3,2,1,1,3]
+    }
+];
+
+module.exports = friendsData;
