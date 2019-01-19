5b1e733ca2146ca448c7f703c588806b2c1ac9b2
cm718 <cm718@icloud.com>
2019-01-18 21:42:08 -0600

    added commenting

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,18 +1,19 @@
 
 
 // Require in the friends data
-const friendData = require('../data/friends');
+let friendsData = require('../data/friends');
 
-// Set up the routes
-
+// Export the routes for other docs
 module.exports = (app) => {
-
+    // GET route for api/friends
     app.get('/api/friends', (req, res)=>{
         res.json(friendsData);
     });
-
-    app.post('/api/friends', (req, res)=>{
-        let 
-    });
-
-}
+    // POST route to update with new data
+    app.post("/api/friends", (req, res) => {
+        // Push in new data from the request body
+        friendsData.push(req.body);
+        // Response of friendsData
+        res.json(friendsData);
+      });
+};
