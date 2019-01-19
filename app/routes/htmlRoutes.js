b11e00f83533ba8267f3f169b94f42a04e7639f2
cm718 <cm718@icloud.com>
2019-01-18 21:42:29 -0600

    updated root path

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -9,8 +9,8 @@ module.exports = (app) => {
         res.sendFile(path.join(__dirname, '../public/survey.html'));
     });
     // Get request to send the home page html to the browswer
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/home.html'));
     });
 
-}
+};
