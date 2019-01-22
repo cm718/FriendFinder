5b470ad2a62d275533ce214f94f15255f7ae84f9
cm718 <cm718@icloud.com>
2019-01-21 21:18:41 -0600

    Updated survey page with for loop

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -9,7 +9,7 @@ module.exports = (app) => {
     });
     // POST route to update with new data
     app.post("/api/friends", (req, res) => {
-
+        console.log('friend post data',req.body);
         // Declare a variable for our best match
         let bestMatch = {
             "name": "",
@@ -19,6 +19,7 @@ module.exports = (app) => {
 
         // Declare variables to hold the user data from POST req
         let userData = req.body;
+        friendsData.push(userData);
         let userScores = userData.scores;
         // Declare variable to hold difference between user score and each other user
         let totalDifference;
