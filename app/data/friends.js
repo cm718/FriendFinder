45645f7d7fb8e62ae7c83c76349064fb2d4c5c12
cm718 <cm718@icloud.com>
2019-01-23 11:32:21 -0600

    added more data to the friends data

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,8 +2,28 @@
 let friendsData = [
     {
         "name": "Chris",
-        "photo": "https://images.google.com",
-        "scores": [5,3,4,3,2,3,2,1,1,3]
+        "photo": "https://ak8.picdn.net/shutterstock/videos/1583458/thumb/3.jpg",
+        "scores": [5,2,4,2,3,2,2,1,1,2]
+    },
+    {
+        "name": "Matt",
+        "photo": "https://media.istockphoto.com/photos/portrait-of-a-handsome-man-smiling-isolated-on-white-picture-id637538262?k=6&m=637538262&s=612x612&w=0&h=J5lue2wanc24qgiXqcE8fBeZkFdI3dNVzA-lSyC3WiQ=",
+        "scores": [5,2,4,2,2,1,1,1,1,2]
+    },
+    {
+        "name": "Ryan",
+        "photo": "https://myrealdomain.com/images/male-stock-photos.png",
+        "scores": [3,2,4,1,1,2,2,1,1,4]
+    },
+    {
+        "name": "Tony",
+        "photo": "https://image.shutterstock.com/mosaic_250/1849259/575234476/stock-photo-good-looking-man-in-glasses-portrait-575234476.jpg",
+        "scores": [3,3,1,5,5,2,2,1,1,2]
+    },
+    {
+        "name": "Sophia",
+        "photo": "https://images.pexels.com/photos/1036622/pexels-photo-1036622.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "scores": [1,5,4,5,2,3,2,5,1,3]
     }
 ];
 
